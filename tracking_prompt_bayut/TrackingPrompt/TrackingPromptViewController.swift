33259f7feb33bfb57dd6bd187461426952f5e722
kanaanzafar7 <kanaan.zafar@empglabs.com>
2021-09-09 15:58:22 +0500

    request tracking permission

--- tracking_prompt_bayut/TrackingPrompt/TrackingPromptViewController.swift
+++ tracking_prompt_bayut/TrackingPrompt/TrackingPromptViewController.swift
@@ -1,12 +1,5 @@
-//
-//  TrackingPromptViewController.swift
-//  tracking_prompt_bayut
-//
-//  Created by Kanaan Zafar on 09/09/2021.
-//
-
 import UIKit
-
+import AppTrackingTransparency
 class TrackingPromptViewController: UIViewController {
     
     @IBOutlet weak var permissionDescriptionLabel: UILabel!
@@ -17,9 +10,14 @@ class TrackingPromptViewController: UIViewController {
         super.viewDidLoad()
         permissionDescriptionLabel.text = "We need your permission to ensure the content we share is relevant and personalized to you.\n\nTap Allow on the next screen to help us personalize your experience."
         continueButton.layer.cornerRadius = 4
-  }
-    
-    @IBAction func onPressedContinue(_ sender: UIButton) {
     }
     
+    @IBAction func onPressedContinue(_ sender: UIButton) {
+        requestPermission()
+    }
+    func requestPermission() {
+        ATTrackingManager.requestTrackingAuthorization { authorizationStatus in
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
 }
