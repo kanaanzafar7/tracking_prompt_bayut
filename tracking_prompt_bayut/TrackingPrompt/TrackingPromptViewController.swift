075330e14eed6dd10cba78f1e1c1aabecafb7c45
kanaanzafar7 <kanaan.zafar@empglabs.com>
2021-09-14 16:28:04 +0500

    font size according to screen

--- tracking_prompt_bayut/TrackingPrompt/TrackingPromptViewController.swift
+++ tracking_prompt_bayut/TrackingPrompt/TrackingPromptViewController.swift
@@ -9,15 +9,22 @@ class TrackingPromptViewController: UIViewController {
     @IBOutlet weak var building: UIImageView!
     @IBOutlet weak var helpUsDescription: UILabel!
     @IBOutlet weak var footerDescription: UILabel!
-    
+    var screenSize: CGSize!
     override func viewDidLoad() {
         super.viewDidLoad()
+        screenSize = UIScreen.main.bounds.size
         continueButton.layer.cornerRadius = 4
+        var fontSize : CGFloat = 16
+        
+        if screenSize.height < 700 || screenSize.width < 400 {
+            fontSize = 14
+        }
+        
         let permissionDescription = "We need your permission to ensure the content we share is relevant and personalized to you.\n\nTap \"Allow\" on the next screen to help us personalize your experience."
         let attributedString = NSMutableAttributedString.init(string: permissionDescription)
         let range = NSString(string: permissionDescription).range(of: "\"Allow\"", options: String.CompareOptions.caseInsensitive)
         attributedString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.black,
-                                        NSAttributedString.Key.font : UIFont(name: "Lato-Bold", size: 16)!
+                                        NSAttributedString.Key.font : UIFont(name: "Lato-Bold", size: fontSize)!
         ], range: range)
         permissionDescriptionLabel.attributedText = attributedString
         addConstraints()
@@ -32,9 +39,8 @@ class TrackingPromptViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
-    
     func addConstraints(){
-        let screenSize =    UIScreen.main.bounds.size
+        
         print("-----screenSize: \(screenSize.height) and \(screenSize.width)")
     }
 }
